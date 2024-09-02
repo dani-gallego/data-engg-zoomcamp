caa4b78b4b3a89cf02ad060bc8f545e056339b4e
Dani G <madaniellagallego@gmail.com>
2024-09-03 04:19:39 +0800

    Update variable.tf

--- 01-introduction/terraform/variable.tf
+++ 01-introduction/terraform/variable.tf
@@ -8,29 +8,29 @@ variable "project" {
 
 variable "region" {
   description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
-  default = "europe-west6"
-  type = string
+  default     = "europe-west6"
+  type        = string
 }
 
 # Not needed for now
 variable "bucket_name" {
   description = "Name of the bucket to be created. Must be unique"
-  default = " "
+  default     = " "
 }
 
 variable "storage_class" {
   description = "Storage class type for your bucket. Check official docs for more info."
-  default = "STANDARD"
+  default     = "STANDARD"
 }
 
 variable "BQ_DATASET" {
   description = "BigQuery Dataset that raw data (from GCS) will be written to"
-  type = string
-  default = "trips_data_all"
+  type        = string
+  default     = "trips_data_all"
 }
 
 variable "TABLE_NAME" {
   description = "BigQuery Table"
-  type = string
-  default = "ny_trips"
+  type        = string
+  default     = "ny_trips"
 }
